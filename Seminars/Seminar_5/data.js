0a96caf0325fe54fb51bdc47e9de782ec9fc4afc
idorry <iannoyinbox@gmail.com>
2024-03-21 02:20:08 +0300

    sdsd

---
+++ Seminars/Seminar_5/data.js
@@ -0,0 +1,23 @@
+const dataInfo =
+    [
+        {
+            "id": "NISSAN_QASHQAI",
+            "name": "Qashqai",
+            "class": "C",
+            "url": "http://www.shutterstock.com/ru/image-photo/otopeni-romania-september-17-2021-2022-2057072357"
+        },
+        {
+            "id": "AUDI_Q7",
+            "name": "Q7",
+            "class": "E",
+            "url": "http://www.istockphoto.com/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%BE%D0%B1%D0%B8%D0%BB%D1%8C-audi-no7-4x4-%D0%BD%D1%8B%D1%80%D1%8F%D0%B5%D1%82-%D1%87%D0%B5%D1%80%D0%B5%D0%B7-%D0%B3%D0%BE%D1%80%D0%BE%D0%B4-%D0%BA%D0%BE%D1%82%D1%81%D1%83%D0%BE%D0%BB%D0%B4-%D0%B2-%D1%81%D0%B8%D1%80%D0%B5%D0%BD%D1%81%D1%82%D0%B5%D1%80%D0%B5-%D0%BD%D0%B0-%D1%82%D1%83%D0%BC%D0%B0%D0%BD%D0%BD%D0%BE%D0%B9-%D1%83%D1%82%D1%80%D0%B5%D0%BD%D0%BD%D0%B5%D0%B9-gm1130648011-299092518"
+        },
+        {
+            "id": "MITSUBISHI_ASPIRE",
+            "name": "Aspire",
+            "class": "D",
+            "url": "http://www.drive2.ru/r/mitsubishi/aspire/4062246863888429889/"
+        }
+    ]
+
+
